946e150f6c747e71347a7b6112f3559adb0850a1
Shane Becker <veganstraightedge@gmail.com>
2019-08-07 11:28:09 -0700

    Use single quotes

--- runbooks/fix_postgres.rb
+++ runbooks/fix_postgres.rb
@@ -1,54 +1,57 @@
 #!/usr/bin/env ruby
-require "runbook"
+require 'runbook'
 
-runbook = Runbook.book "Fix Postgres" do
-  description "This runbook is for when postgres doesn't seem to be working"
+runbook = Runbook.book 'Fix Postgres' do
+  description 'This runbook is for when postgres doesn’t seem to be working'
 
-  section "Check postgres is installed" do
-    step "Check brew installation" do
-      note "brew info postgresql"
+  section 'Check Postgres is installed' do
+    step 'Check brew installation' do
+
+      # command %q{brew info postgresql | grep 'installed'}
+
+      # note 'brew info postgresql'
     end
 
-    step "Check psql version" do
-      note "psql --version"
+    step 'Check psql version' do
+      note 'psql --version'
     end
 
-    step "Check psql location" do
-      note "which psql"
-    end
-  end
-
-  section "Check postgres is running" do
-    step "Check brew services" do
-      note "brew services list"
-      note "Does it say 'started' in yellow?"
-    end
-
-    step "Check postgres is ready" do
-      note "pg_isready"
+    step 'Check psql location' do
+      note 'which psql'
     end
   end
 
-  section "Is postmaster.pid the culprit?" do
-    step "Check for postmaster.pid" do
-      note "cat /usr/local/var/postgresql/postmaster.pid"
+  section 'Check postgres is running' do
+    step 'Check brew services' do
+      note 'brew services list'
+      note 'Does it say ‘started’ in yellow?'
     end
 
-    step "Delete postmaster.pid" do
-      note "rm /usr/local/var/postgresql/postmaster.pid"
+    step 'Check postgres is ready' do
+      note 'pg_isready'
+    end
+  end
+
+  section 'Is postmaster.pid the culprit?' do
+    step 'Check for postmaster.pid' do
+      note 'cat /usr/local/var/postgresql/postmaster.pid'
     end
 
-    step "Check brew services again" do
-      note "brew services list"
+    step 'Delete postmaster.pid' do
+      note 'rm /usr/local/var/postgresql/postmaster.pid'
     end
 
-    step "Restart postgres" do
-      note "brew services restart postgresql"
+    step 'Check brew services again' do
+      note 'brew services list'
     end
 
-    step "Double confirmation" do
-      note "brew services list"
-      note "pg_isready"
+    step 'Restart postgres' do
+      note 'brew services restart postgresql'
+    end
+
+    step 'Double confirmation' do
+      note 'brew services list'
+      note 'pg_isready'
     end
   end
 end
