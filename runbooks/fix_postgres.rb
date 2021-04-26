a484b5fd28be4f583f2e3a76e35aca7f32c837f1
Shane Becker <veganstraightedge@gmail.com>
2021-04-26 16:25:11 -0700

    Add another lookup path for postmaster.pid

--- runbooks/fix_postgres.rb
+++ runbooks/fix_postgres.rb
@@ -40,10 +40,12 @@ runbook = Runbook.book 'Fix Postgres' do
   section 'Is postmaster.pid the culprit?' do
     step 'Check for postmaster.pid' do
       note 'cat /usr/local/var/postgresql/postmaster.pid'
+      note 'cat /usr/local/var/postgresq/postmaster.pid'
     end
 
     step 'Delete postmaster.pid' do
       note 'rm /usr/local/var/postgresql/postmaster.pid'
+      note 'rm /usr/local/var/postgresq/postmaster.pid'
     end
 
     step 'Check brew services again' do
